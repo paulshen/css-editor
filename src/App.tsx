7e0a6d35028253ca104b6499da6fc4374a13d870
Paul Shen <paulshen0@gmail.com>
2020-06-13 09:51:25 -0700

    css-property suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -85,6 +85,60 @@ function CSSPropertyElement(props: RenderElementProps) {
   const editor = useEditor();
   const selected = useSelected();
   const { attributes, children, element } = props;
+  const childText = element.children[0].text as string;
+  const suggestions = React.useMemo(() => {
+    if (!selected) {
+      return undefined;
+    }
+    if (element.value !== undefined) {
+      return undefined;
+    }
+    return Object.keys(ENUM_PROPERTIES).filter((option) =>
+      option.startsWith(childText)
+    );
+  }, [selected, element.value, childText]);
+  const hasSuggestions = suggestions !== undefined && suggestions.length > 0;
+  const [selectedSuggestionIndex, setSelectedSuggestionIndex] = React.useState(
+    0
+  );
+  const selectedSuggestionIndexRef = React.useRef(selectedSuggestionIndex);
+  React.useEffect(() => {
+    selectedSuggestionIndexRef.current = selectedSuggestionIndex;
+  });
+  React.useEffect(() => {
+    if (suggestions !== undefined && suggestions.length > 0) {
+      editor.suggestionsHandleKeyEnter = (e: KeyboardEvent) => {
+        const value = suggestions[selectedSuggestionIndexRef.current];
+        const [nodeEntry] = Editor.nodes(editor, {
+          match: (node) => node === element,
+        });
+        const [_, nodePath] = nodeEntry;
+        Transforms.setNodes(editor, { value }, { at: nodePath });
+        Transforms.delete(editor, { at: [...nodePath, 0] });
+        const valueNodePath = Path.next(nodePath);
+        Transforms.setNodes(editor, { property: value }, { at: valueNodePath });
+        const valueNodePoint = { path: valueNodePath, offset: 0 };
+        Transforms.setSelection(editor, {
+          anchor: valueNodePoint,
+          focus: valueNodePoint,
+        });
+        e.preventDefault();
+      };
+      editor.suggestionsHandleKeyArrow = (e: KeyboardEvent) => {
+        const key = e.key;
+        setSelectedSuggestionIndex(
+          (index) =>
+            (index + (key === "ArrowUp" ? -1 : 1) + suggestions.length) %
+            suggestions.length
+        );
+        e.preventDefault();
+      };
+      return () => {
+        editor.suggestionsHandleKeyEnter = undefined;
+        editor.suggestionsHandleKeyArrow = undefined;
+      };
+    }
+  }, [suggestions]);
   React.useEffect(() => {
     if (!selected && element.value === undefined) {
       const childText = element.children[0].text;
@@ -126,6 +180,23 @@ function CSSPropertyElement(props: RenderElementProps) {
       <span contentEditable={false} className={styles.colon}>
         :{" "}
       </span>
+      {hasSuggestions ? (
+        <div className={styles.suggestions} contentEditable={false}>
+          {suggestions!.map((suggestion, i) => (
+            <div
+              className={
+                styles.suggestionListItem +
+                (selectedSuggestionIndex === i
+                  ? " " + styles.suggestionListItemSelected
+                  : "")
+              }
+              key={suggestion}
+            >
+              {suggestion}
+            </div>
+          ))}
+        </div>
+      ) : null}
     </span>
   );
 }
@@ -149,8 +220,11 @@ function CSSValueElement(props: RenderElementProps) {
     if (typeof element.property !== "string") {
       return undefined;
     }
-    const foo = ENUM_PROPERTIES[element.property];
-    return foo.filter((option) => option.startsWith(childText));
+    const enumValues = ENUM_PROPERTIES[element.property];
+    if (enumValues === undefined) {
+      return undefined;
+    }
+    return enumValues.filter((option) => option.startsWith(childText));
   }, [selected, element.property, element.value, childText]);
   const hasSuggestions = suggestions !== undefined && suggestions.length > 0;
   const [selectedSuggestionIndex, setSelectedSuggestionIndex] = React.useState(
@@ -169,7 +243,11 @@ function CSSValueElement(props: RenderElementProps) {
         });
         const [_, nodePath] = nodeEntry;
         Transforms.setNodes(editor, { value }, { at: nodePath });
-        Transforms.delete(editor, { at: [...nodePath, 0] });
+        Transforms.delete(editor, { at: [...nodePath, 0], voids: true });
+        Transforms.setSelection(editor, {
+          anchor: { path: nodePath, offset: 0 },
+          focus: { path: nodePath, offset: 0 },
+        });
         e.preventDefault();
       };
       editor.suggestionsHandleKeyArrow = (e: KeyboardEvent) => {
@@ -202,7 +280,7 @@ function CSSValueElement(props: RenderElementProps) {
       ) : null}
       {children}
       {hasSuggestions ? (
-        <div className={styles.cssValueSuggestions} contentEditable={false}>
+        <div className={styles.suggestions} contentEditable={false}>
           {suggestions!.map((suggestion, i) => (
             <div
               className={
